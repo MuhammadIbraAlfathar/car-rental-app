bf5843e81bbb323d727b99d0d90c6be5ef818273
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-10-14 16:52:05 +0700

    chore: setup Docker environment for development
    Added dockerfile and docker-compose.yml for containerizing application. Configured services for app and PostgreSQL database.

---
+++ cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/MuhammadIbraAlfathar/car-rental-app/config"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	godotenv.Load()
+	config.LoadEnv()
+	_, err := config.NewPostgres()
+	if err != nil {
+		log.Println("ERROR TO CONNECT DATABASE")
+	}
+	r.Run()
+}
